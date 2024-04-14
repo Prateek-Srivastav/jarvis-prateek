5fbc92f53cb8b45ac0e1cf0cb40b1fd885c8e6b8
Prateek Srivastav <prateek.srvastav@gmail.com>
2024-04-15 00:33:31 +0530

    updated script file

--- backend/jarvis-backend.sh
+++ backend/jarvis-backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the directory containing your Node.js application
-cd /home/prateek/Code/backends/jarvis
+cd /home/prateek/Code/jarvis-prateek/backend
 
 # Start the Node.js process
 node index.js &
